5557dc0a94157a585a7a17703c6d1231ca95357d
anotherbondan <anandagautama3112@gmail.com>
2026-02-18 19:46:09 +0700

    chore: disable CORS with credentials support

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3001);
